6bf3defc14660acd97a08e62f48eade3c3adcf88
Anastasia Mikheeva <nast967@gmail.com>
2023-07-31 15:29:36 +0300

    2

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const path = require('path')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `@import "src/assets/scss/main.scss";`
-      }
-    }
-  }
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+  },
 })
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/imports.styl'),
+      ],
+    })
+}
 
 

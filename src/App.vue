42d4fbd98274c690d8bb0091eb811a420a1e9e28
Anastasia Mikheeva <nast967@gmail.com>
2023-10-19 17:58:06 +0300

    loader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app">
+    <PreloaderPage  />
     <AppNav />
       <router-view></router-view>
     <AppFooter />
@@ -9,15 +10,23 @@
 <script>
 import AppNav from '@/layout/AppNav.vue'
 import AppFooter from '@/layout/AppFooter.vue'
+import PreloaderPage from './layout/PreloaderPage.vue'
 
 export default {
   name: 'App',
   components: {
     AppNav,
-    AppFooter
+    AppFooter,
+    PreloaderPage
   },
   mounted() {
     localStorage.setItem(this.$route.path, true)
   },
 }
 </script>
+
+<style>
+.app {
+  position: relative;
+}
+</style>
